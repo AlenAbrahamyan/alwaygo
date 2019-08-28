eaf83629943fd85a96e7e52111b0005791777e58
unknown <alenabrahamyan0@gmail.com>
2019-08-29 03:14:59 +0400

    first commit

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+import Verify from './components/Verify';
+
+import './style/authenticated.css';
+import './style/welcome.css';
+
+import { Provider } from 'react-redux';
+import store from './store';
+import { loadUser } from './actions/authActions';
+import { Route, BrowserRouter } from 'react-router-dom'
+
+
+class App extends Component {
+
+  componentDidMount() {
+    store.dispatch(loadUser());
+  }
+
+  render() {
+
+    return (
+
+
+      <Provider store={store}>
+        <div>
+          <Verify />
+        </div>
+      </Provider>
+    )
+  }
+}
+
+export default App;
